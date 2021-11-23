f1d5872746c3b40eb551cd8e530ed9ffcdd2e498
Osei Pinamang Kwadwo <osei.pinamang@gmail.com>
2021-11-23 13:56:37 +0000

    Add main.js

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const items = ["Rock", "Paper", "Scissors"];
+const items = ["Rock", "Paper", "Scissors", "Mango", "Juice"];
 function computerPlay(){
     return items[Math.floor(Math.random()*items.length)];
 }
 // console.log(computerPlay(items))
+
+const playerSelection = prompt("enter one item", items);
+ const computerSelection = computerPlay();
+
+function playOn(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "It's a tie!"
+    }
+    else{
+        return `${playerSelection} beats ${computerSelection} and wins the game`
+                }
+}
+                
+    console.log(playOn(playerSelection,computerSelection)); 
+
+    
