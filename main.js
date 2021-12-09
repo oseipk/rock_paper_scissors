c1b537896e289e85d085708f4deb95c978dda8e9
Osei Pinamang Kwadwo <osei.pinamang@gmail.com>
2021-12-09 11:47:55 +0000

    Add main.js

--- main.js
+++ main.js
@@ -2,23 +2,36 @@
 const items = ["Rock", "Paper", "Scissors"];
 
 // computer automatically select randomly from items
+
+
 function computerPlay(){
-    return items[Math.floor(Math.random()*items.length)];
-}
-
-
+    return items[Math.floor(Math.random()*items.length)]
+}; 
 const playerSelection = prompt("enter one item", items);
+playerSelection.toLowerCase();
 const computerSelection = computerPlay();
 
+//play game
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){
-        return "It's a tie!"
+    if(playerSelection !== computerSelection){
+        return "You Lose !" ${playerSelection} beats ${computerSelection}
+    }else {
+        return "it's a tie"
+
+         
+function game(){
+    for(let i=5; i>items.length; i--){
+        if(playerSelection===computerSelection){
+            score1==0;
+        }else{
+            score2 ==1;
+        }
+        const score = score1 + score 2;
+        
     }
-    else{
-        return `${playerSelection} beats ${computerSelection} and wins the game`
-         }
 }
+
                 
-    console.log(playRound(playerSelection,computerSelection)); 
+ //   console.log(playRound(playerSelection,computerSelection)); 
 
      
